432c159807fedbdf9c896ca6090b94238d48388b
Krishn Shekhawat <107538012+krishn011@users.noreply.github.com>
2023-03-26 11:42:21 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@conversionFunctions/main.c
@@ -0,0 +1,19 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+
+int main() {
+    const char *ip = "192.168.0.1";
+    struct in_addr ip_addr;
+    
+    if (inet_aton(ip, &ip_addr)) {
+        printf("Indirizzo IP in formato binario: %x\n", ip_addr.s_addr);
+    } else {
+        printf("Indirizzo IP non valido\n");
+    }
+
+  uint16_t host_short = 0x1234;
+    uint16_t net_short = htons(host_short);
+    printf("Valore intero a 16 bit nel formato di rete: %x\n", net_short);
+  
+    return 0;
+}
